656eef0f5bbb9ce43a0084d66dc0e83d7d2f6cec
sunlulu427 <sun_lulu427@163.com>
2020-11-18 02:29:27 +0800

    1.1.1

---
+++ app/src/main/java/com/example/demo/view/BaseView.kt
@@ -0,0 +1,31 @@
+package com.example.demo.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * @author: sunlulu.tomato@bytedance.com
+ * @date:   2020/11/18 2:07 AM
+ **/
+class BaseView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : View(context, attrs, defStyle) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        // 设置画笔的基本属性
+        val paint = Paint().apply {
+            color = Color.RED
+            style = Paint.Style.STROKE
+            strokeWidth = 50f
+        }
+
+        // 画圆
+        canvas?.drawCircle(190f, 200f, 150f, paint)
+    }
+}
