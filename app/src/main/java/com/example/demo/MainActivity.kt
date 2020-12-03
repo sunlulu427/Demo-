9ce1e14ed706415ce6ddbe9f0da1d38ea26d6474
sunlulu427 <sun_lulu427@163.com>
2020-12-04 02:22:10 +0800

    视图动画

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start_anim.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.scaleanim)
+            tv.startAnimation(animation)
+        }
     }
 }
