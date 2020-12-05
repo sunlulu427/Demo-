7ad9d8d553c6f1559c9117bdc2657bbbb4be7b38
sunlulu427 <sun_lulu427@163.com>
2020-12-05 23:20:54 +0800

    缩放动画

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.view.animation.ScaleAnimation
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         start_anim.setOnClickListener {
-            val animation = AnimationUtils.loadAnimation(this, R.anim.scaleanim)
+            val animation = ScaleAnimation(0f, 1.4f, 0f, 1.4f,
+                Animation.RELATIVE_TO_SELF, .5f, Animation.RELATIVE_TO_SELF, .5f).also {
+                it.duration = 700
+            }
             tv.startAnimation(animation)
         }
     }
