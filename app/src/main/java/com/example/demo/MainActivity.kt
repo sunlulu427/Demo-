0a4ddcf106e079712c600d2be850152ce8e4cb5d
sunlulu427 <sun_lulu427@163.com>
2020-12-06 23:52:01 +0800

    ValueAnimator

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.demo
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.*
+import com.example.demo.util.normalToast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         start_anim.setOnClickListener {
-            val animationSet = AnimationUtils.loadAnimation(this, R.anim.scaleanim).also {
-                it.interpolator = AccelerateDecelerateInterpolator()
+            val animator = ValueAnimator.ofInt(0, 400).also {
+                it.duration = 1000
             }
-            tv.startAnimation(animationSet)
+            animator.addUpdateListener {
+                val curValue = it.animatedValue as Int
+                tv.layout(curValue, curValue, curValue + tv.width, curValue + tv.height)
+            }
+            animator.start()
+        }
+
+        tv.setOnClickListener {
+            normalToast("click me")
         }
     }
 }
