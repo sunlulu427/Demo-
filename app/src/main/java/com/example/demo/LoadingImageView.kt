cd0d88af28716895ceaef21007490e3bbcc33590
sunlulu427 <sun_lulu427@163.com>
2020-12-07 23:10:19 +0800

    属性动画

---
+++ app/src/main/java/com/example/demo/LoadingImageView.kt
@@ -0,0 +1,71 @@
+package com.example.demo
+
+import android.animation.Animator
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.animation.AccelerateDecelerateInterpolator
+
+/**
+ * @author: sunlulu.tomato@bytedance.com
+ * @date:   2020/12/7 10:40 PM
+ **/
+class LoadingImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyle) {
+
+    companion object {
+        private const val IMAGE_COUNT = 3
+    }
+
+    private var mTop = 0
+    private var mCurImageIndex = 0
+
+    init {
+        val valueAnimator = ValueAnimator.ofInt(0, 100, 0).also {
+            it.repeatCount = ValueAnimator.INFINITE
+            it.repeatMode = ValueAnimator.RESTART
+            it.duration = 2000
+            it.interpolator = AccelerateDecelerateInterpolator()
+            it.addUpdateListener { animator ->
+                val dx = animator.animatedValue as Int
+                top = mTop - dx
+            }
+        }
+
+        valueAnimator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+                mCurImageIndex ++
+                setImageDrawable(when (mCurImageIndex % IMAGE_COUNT) {
+                    0 -> resources.getDrawable(R.drawable.x1)
+                    1 -> resources.getDrawable(R.drawable.x2)
+                    2 -> resources.getDrawable(R.drawable.x3)
+                    else -> resources.getDrawable(R.drawable.x4)
+                })
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        valueAnimator.start()
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        mTop = top
+    }
+}
