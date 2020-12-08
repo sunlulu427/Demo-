9803c5b1baef929bf188044374eaaea092f348a1
sunlulu427 <sun_lulu427@163.com>
2020-12-09 00:35:48 +0800

    属性动画

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.demo
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start_anim.setOnClickListener {
+            val animator = ObjectAnimator.ofFloat(image, "rotation", 0f, 180f, 0f)
+            animator.duration = 5000
+            animator.start()
+        }
     }
 }
