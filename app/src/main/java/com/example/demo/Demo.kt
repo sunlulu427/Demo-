4746285c4d6d616fad09354cd026af6f6539407c
sunlulu427 <sun_lulu427@163.com>
2020-12-18 02:24:18 +0800

    orElse PartialFunction

--- app/src/main/java/com/example/demo/Demo.kt
+++ app/src/main/java/com/example/demo/Demo.kt
@@ -1,39 +1,53 @@
+import java.lang.IllegalArgumentException
+
 data class ApplyEvent(val money: Int, val title: String)
 
-interface ApplyHandler {
-    val successor: ApplyHandler?
-    fun handleEvent(event: ApplyEvent)
+class PartialFunction<in P1, out R>(
+    private val definedAt: (P1) -> Boolean,
+    private val f: (P1) -> R
+): (P1) -> R {
+    override fun invoke(p1: P1): R {
+        if (definedAt(p1)) {
+            return f(p1)
+        }
+        throw IllegalArgumentException("Value: $p1 isn't supported by this function")
+    }
+
+    fun isDefinedAt(p1: P1) = definedAt(p1)
 }
 
-class GroupLeader(override val successor: ApplyHandler?): ApplyHandler {
-    override fun handleEvent(event: ApplyEvent) {
+infix fun <P1, R> PartialFunction<P1, R>.orElse(that: PartialFunction<P1, R>): PartialFunction<P1, R> {
+    return PartialFunction({ this.isDefinedAt(it) || that.isDefinedAt(it)}) {
         when {
-            event.money <= 100 -> println("Group leader handled application: ${event.title}")
-            else -> when (successor) {
-                is ApplyHandler -> successor.handleEvent(event)
-                else -> println("Group Leader: This application can not be handled.")
-            }
+            this.isDefinedAt(it) -> this(it)
+            else -> that(it)
         }
     }
 }
+// 自运行lambda
+val groupLeader = {
+    val definedAt: (ApplyEvent) -> Boolean = { it.money <= 200 }
+    val handler: (ApplyEvent) -> Unit = {
+        println("Group Leader handled application: ${it.title}")
+    }
+    PartialFunction(definedAt, handler)
+}()
 
-class President(override val successor: ApplyHandler?): ApplyHandler {
-    override fun handleEvent(event: ApplyEvent) {
+val president = {
+    val definedAt: (ApplyEvent) -> Boolean = { it.money <= 500 }
+    val handler: (ApplyEvent) -> Unit = {
+        println("President handled application: ${it.title}")
+    }
+    PartialFunction(definedAt, handler)
+}()
+
+val college = {
+    val definedAt: (ApplyEvent) -> Boolean = { true }
+    val handler: (ApplyEvent) -> Unit = {
         when {
-            event.money <= 500 -> println("President handled application: ${event.title}")
-            else -> when (successor) {
-                is ApplyHandler -> successor.handleEvent(event)
-                else -> println("President: This application can not be handled.")
-            }
+            it.money > 1000 -> println("College: This application is refused.")
+            else -> println("College handled application ${it.title}")
         }
     }
-}
-
-class College(override val successor: ApplyHandler?): ApplyHandler {
-    override fun handleEvent(event: ApplyEvent) {
-        when {
-            event.money > 1000 -> println("College: This application is refused.")
-            else -> println("College handled application ${event.title}.")
-        }
-    }
-}
+    PartialFunction(definedAt, handler)
+}()
